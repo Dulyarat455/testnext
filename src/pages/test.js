fbb28dc7f357557f5c32c0f3b45f5e55bdf5365a
Dulyarat455 <Dulyarat.455@gmail.com>
2024-06-20 10:43:46 +0700

    edit test

--- src/pages/test.js
+++ src/pages/test.js
@@ -13,6 +13,9 @@ export default function Home() {
 
     const [base64, setBase64] = useState('');
     const [isHovered, setIsHovered] = useState(false);
+    const [message, setMessage] = useState("Upload image to clound success");
+    const [messagestatus, setMessagestatus] = useState(true);
+    const [info, setInfo] = useState({ test:" " }); 
   
     const handleFileChange = async (event) => {
       const file = event.target.files[0];
@@ -32,6 +35,34 @@ export default function Home() {
       });
     };
 
+    const uploadImage = () =>{
+      const res = fetch("/api/sentimage", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(info),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.success) {
+            console.log(data.success);
+
+            setMessage(data.message);
+            setMessagestatus(true);
+
+            setTimeout(() => {
+              router.push("/workspace_dailyexpense");
+            }, 1000);
+          } else {
+            console.log(data);
+            setMessage(data.message);
+            setMessagestatus(false);
+          }
+        });
+
+    }
+
 
   return (
     <div>
@@ -43,11 +74,23 @@ export default function Home() {
       onMouseLeave={() => setIsHovered(false)}>
       <Image src={isHovered ? drive1 : Remove} alt="user"   className="cursor-pointer" style={{ width: 'auto', height: 'auto' }}/>
     </div>
+    <div className="flex justify-center items-center ml-3 mb-4">
+      {message && (
+                <p
+                  className={`${
+                    messagestatus ? "text-green-400" : "text-red-500"
+                  } w-full text-left text-sm`}
+                >
+                  {message}
+                </p>
+              )}
+    </div>
     <div className="flex space-x-4">
 
       <button className="bg-blue-900 text-white py-2 px-4 rounded" onClick={
         ()=>{
           setIsHovered(!isHovered);
+          uploadImage();
         }
       }>
         Uploaded
@@ -60,13 +103,15 @@ export default function Home() {
     
       
       <input type="file" onChange={handleFileChange} />
-      <button onClick={() => alert(base64)}>Show Base64</button>
+      <button onClick={() => {alert(base64)
+        console.log(base64)}
+      }>Show Base64</button>
       {base64 && (
         <div>
-          <h3>Base64 Output:</h3>
+          {/* <h3>Base64 Output:</h3>
           <textarea value={base64} readOnly rows="10" cols="50"
           className="p-2 border border-gray-700 rounded bg-gray-800 text-white"
-          />
+          /> */}
           <img src={base64} alt="Uploaded Image" style={{ maxWidth: '300px', marginTop: '10px' }} />
         </div>
       )}
